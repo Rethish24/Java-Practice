5c5dcb3b33f3dd6408f128e6fd5057ac35330310
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-03 17:42:41 +0000

    commit

---
+++ Strings/reverse_string_word_wise.java
@@ -0,0 +1,33 @@
+package Strings;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class reverse_string_word_wise {
+   
+    public static String reverseWords(String str)
+    {
+        // your code here
+        String [] res = str.split(" ");
+        StringBuilder s= new StringBuilder();
+        int n=res.length;
+        for(int i=n-1;i>=0;i--)
+        {  
+            s.append(res[i]);
+            s.append(" ");
+        }
+        return s.toString();
+    } 
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        String p = sc.nextLine();
+        while (t > 0) {
+            String s = sc.nextLine();
+            System.out.println(reverseWords(s));
+            t--;
+        }
+    }
+}
