d28776973d2ad373b1faf2fd48f9812fa3416721
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-03 17:40:01 +0000

    commit

---
+++ Strings/Autori.java
@@ -0,0 +1,45 @@
+package Strings;
+
+/*
+Input
+
+Knuth-Morris-Pratt
+Output
+
+KMP
+ */
+import java.io.*;
+import java.util.*;
+
+public class Autori{
+	static String autori(String str)
+    {
+        // Your code here
+        //str =str.split("-");
+        char prev=' ';
+        String s="";
+        s=s+str.charAt(0);
+        int n=str.length();
+        for(int i=1;i<n;i++)
+        {
+           char cur=str.charAt(i);
+           if(cur>='A' && cur<='Z' )
+           {
+                  if(prev=='-')
+                  { 
+                    s=s+cur;
+                  }
+             
+           }
+           prev=cur;  
+        }
+    return s;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str=sc.next();
+        String result = autori(str);
+        System.out.print(result);        
+        System.out.println('\n');
+    }
+}
