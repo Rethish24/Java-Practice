9d8f5f72eedb2f142382df421b7ae37f592d4a8c
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-04 14:17:05 +0000

    commit

---
+++ StringBuilder/ultr_fast_mathematician.java
@@ -0,0 +1,36 @@
+package StringBuilder;
+
+import java.util.Scanner;
+
+public class ultr_fast_mathematician 
+{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String A=scn.nextLine();
+        String B=scn.nextLine();
+        StringBuilder a= new StringBuilder(A);
+        StringBuilder b= new StringBuilder(B);
+        StringBuilder c=solve(a,b);
+        System.out.println(c);
+    }
+    static StringBuilder solve(StringBuilder a, StringBuilder b)
+    {
+        // your code here
+        StringBuilder c  = new StringBuilder();
+        int n =a.length();
+        for(int i=0;i<n;i++)
+        {
+              if(a.charAt(i)!=b.charAt(i))
+              {
+                c.append(1);
+              }
+              else
+              {
+                c.append(0);
+              }
+              
+        }
+        return c;
+    }
+
+}
