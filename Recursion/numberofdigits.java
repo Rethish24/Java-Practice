5adb52f257f8e8fa583d328840b6753151d766f2
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-11 17:21:58 +0000

    commit

---
+++ Recursion/numberofdigits.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class numberofdigits 
+{
+    static int numOfDigi(int n) 
+    {
+        //Write code here
+        if(n==0)
+        {
+            return 0;
+        }
+        int r=numOfDigi(n/10);
+        return r+1;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int result = numOfDigi(n);
+        System.out.println(result);
+        sc.close();
+    }
+}
