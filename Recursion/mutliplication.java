a37a80a55adf9ceb9640ca453d7797d6c48ea952
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-11 14:46:08 +0000

    commit

---
+++ Recursion/mutliplication.java
@@ -0,0 +1,26 @@
+package Recursion;
+import java.util.Scanner;
+
+public class mutliplication 
+{
+    
+    static int sum=0;
+    public static void mul(int n,int m)
+    {
+        if(m==0)
+        {
+            System.out.println(sum);
+            return;
+        }
+        sum=sum+n;
+        mul(n,m-1);
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        mul(n,m);
+     }
+    }
---
+++ twoDArrays/Toeplitz_matrix.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+        public boolean isToeplitzMatrix(int[][] a, int m, int n) 
+       {
+          // write code here
+          for(int i=0;i<m-1;i++)
+          {
+            for(int j=0;j<n-1;j++)
+            {
+                if(a[i][j]!=a[i+1][j+1])
+                {
+                    return false;
+                }
+            }
+          }
+          return true;
+       }
+}
+
+public class Toeplitz_matrix {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m, n;
+        m = sc.nextInt();
+        n = sc.nextInt();
+        int[][] A = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+        Solution Obj = new Solution();
+        boolean ans = Obj.isToeplitzMatrix(A,m,n);
+        System.out.println(ans);
+    }
+}
