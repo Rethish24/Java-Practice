13c17400d85100f5237e8a9c532257009dbeea57
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-12 16:47:26 +0000

    COMMIT

---
+++ Recursion_2D/pair_star.java
@@ -0,0 +1,30 @@
+package Recursion_2D;
+public class pair_Star 
+{
+    static String PairStar(String str,int i,StringBuilder sb) 
+    {
+        //Write your code here
+        if(i==str.length()-1)
+        {
+            sb.append(str.charAt(i));
+            return sb.toString();
+        }
+        sb.append(str.charAt(i));
+        if(str.charAt(i)==str.charAt(i+1))
+        {
+            sb.append("*");
+        }
+        String s=PairStar(str,i+1,sb);
+        return s;
+
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        System.out.println(PairStar(s,0,sb));
+    }
+}
+
+    
