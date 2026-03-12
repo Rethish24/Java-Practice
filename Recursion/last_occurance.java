84de568485dc8e3208c80271df98ddfd282b1c12
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-12 17:08:55 +0000

    commit

---
+++ Recursion/last_occurance.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class last_occurance 
+{
+    public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+		int[] A = new int[N];
+		for(int i = 0; i < N; i++){
+			A[i] = s.nextInt();
+		}
+		int T = s.nextInt();
+		System.out.println(lastIndex(A, T, A.length-1));
+	}
+
+	static int lastIndex(int A[],int T,int k)
+	{
+		//Write your code here
+		if(k<0)
+		{
+			return -1;
+		}
+		if(A[k]==T)
+		{
+			return k;
+		}
+		int r=lastIndex(A,T,k-1);
+		return r;
+       
+	}
+}
+    
