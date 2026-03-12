2b41c09582a086ac09879793fc2347ff50f8cc18
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-12 16:23:14 +0000

    commit

---
+++ Recursion/count_x_in_string.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class count_x_in_string 
+{
+static int countX(String str, int i, int n) 
+    {
+        //Write your code here
+        if(i>str.length()-1)
+        {
+            return 0;
+        }
+        int r=countX(str,i+1,n);
+        return r+((str.charAt(i)=='x')?1:0);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n=str.length();
+        System.out.println(countX(str,0,n));
+    }    
+}
