6bdfac87586a1e5524183b0470d9b8f7e1ef6872
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-14 10:34:58 +0000

    commit

--- recursion_on_strings_2d_array/staris_path.java
+++ recursion_on_strings_2d_array/staris_path.java
@@ -1,5 +1,32 @@
-package recursion_on_strings_&_2d_array;
+package recursion_on_strings_2d_array;
 
-public class staris_path {
-    
+import java.util.Scanner;
+
+
+
+public class staris_path 
+{
+static void printStairPaths(int n,String s) 
+    {
+        //Write your code here
+        if(n==0)
+        {
+            System.out.println(s);
+            return ;
+        }
+        if(n<0)
+        {
+            return ;   
+        }
+       printStairPaths(n-1,s+"1");
+       printStairPaths(n-2,s+"2");
+       printStairPaths(n-3,s+"3");
+        
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        printStairPaths(n,"");
+    }    
 }
