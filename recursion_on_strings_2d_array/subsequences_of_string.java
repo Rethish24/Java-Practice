a6b4859d30dfbcf7ab7cf86d5cd01a17435619e8
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-16 10:06:34 +0000

    commit

---
+++ recursion_on_strings_2d_array/subsequences_of_string.java
@@ -0,0 +1,19 @@
+package recursion_on_strings_2d_array;
+import java.util.Scanner;
+public class subsequences_of_string 
+{
+ static void printSubsequence(String s,String asf,int i)      
+{            
+                if(i>s.length()-1)         
+                 {  System.out.print(asf+" ");            
+                    return ;        
+                 } 
+            printSubsequence(s,asf+s.charAt(i),i+1);         
+            printSubsequence(s,asf,i+1);     
+   }     
+            public static void main(String args[])
+             {         Scanner sc = new Scanner(System.in);        
+                        String s = sc.nextLine();         
+                        printSubsequence(s,"",0);     
+             }     
+}
