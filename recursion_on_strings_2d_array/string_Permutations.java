b06d468749731266513d7f7e148c4541335dd2b3
Rethish24 <rethish.katdhuri3@gmail.com>
2026-03-19 14:14:52 +0000

    commit

---
+++ recursion_on_strings_2d_array/string_Permutations.java
@@ -0,0 +1,41 @@
+package recursion_on_strings_2d_array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class string_Permutations 
+{
+public void printPermutations(String str,String res,ArrayList<String> ans) 
+    {
+
+        // write your code here
+        if(str.length()==0)
+        {
+            if(!ans.contains(res))
+            {
+                ans.add(res);
+                System.out.println(res);
+            }
+            
+            return ;
+        }
+        for(int i=0;i<str.length();i++)
+        {
+            char ch =str.charAt(i);
+            String rem = ((str.substring(0,i))+(str.substring(i+1)));
+            printPermutations(rem,res+ch,ans);                                                         
+        }
+
+    }
+
+
+ 
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        string_Permutations Obj = new string_Permutations();
+        ArrayList<String> ans = new ArrayList<>();
+        Obj.printPermutations(str,"",ans);
+    }    
+}
