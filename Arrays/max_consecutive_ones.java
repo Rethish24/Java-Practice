bae23c292f53108e598c3f3aabb7556fdb05d1b8
Rethish24 <rethish.katdhuri3@gmail.com>
2026-06-28 22:36:17 +0530

    Create max_consecutive_ones.java

---
+++ Arrays/max_consecutive_ones.java
@@ -0,0 +1,26 @@
+
+  class max_consecutive_ones{
+    public int findMaxConsecutiveOnes(int[] nums) 
+    {
+    int count=0;
+    int max=0;
+    for(int i=0;i<nums.length;i++)
+    {
+        
+        if(nums[i]==1)
+        {
+            count++;
+        }
+        else if(nums[i]==0)
+        {
+            max=Math.max(max,count);
+            count=0;
+        }
+    }
+    if(count>max)
+    {
+        max=count;
+    }
+    return max;
+    }
+}
